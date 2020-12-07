cd46e1243dbbf8e6bdafac16bc810cae4429ba97
azizzouaghi <2181@holbertonschool.com>
2020-12-07 22:04:06 +0100

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:9]))
-print("Middle word: {}".format(word[1:8]))
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("First 3 letters: {}"
+      .format(word[0:3]))
+print("Last 2 letters: {}"
+      .format(word[7:9]))
+print("Middle word: {}"
+      .format(word[1:8]))
