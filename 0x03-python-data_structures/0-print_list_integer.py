ccbcfa5541766f64feef997dc8565bbf7212465f
azizzouaghi <2181@holbertonschool.com>
2020-12-15 18:00:06 +0100

    print list integer

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    print ((my_list [0]))
+    print ((my_list [1]))
+    print ((my_list [2]))
+    print ((my_list [3]))
+    print ((my_list [4]))
+    str.format("")
